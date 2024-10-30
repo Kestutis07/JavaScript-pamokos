6511800fb9ab1fe1f3b58974dd428c81c0ea61c0
admin <admin@Vtdks-MBP.lan>
2024-10-30 21:44:52 +0200

    Initial commit

---
+++ pirma-pamoka/script.js
@@ -0,0 +1,21 @@
+console.log(2);
+console.log("Kestutis");
+console.log(1000);
+//   eror
+console.error("Klaida");
+//   cleat
+console.clear();
+//   Math
+console.log(1 + 1);
+//   info
+console.info("Informacija");
+//   table
+console.table(["aple", "banana", "orange"]);
+//   warn
+console.warn("Ispejimas");
+//   Time(padeda pamatuoti kiek laiko uztrunka vykdyti koda)
+console.time();
+//   Kodas kuri skaiciuoja
+console.timeEnd();
+
+// document.getElementById("heading").innerHTML = "Hello world!";
