ddfcd26f64895724bf2288019066e44635a987f2
admin <admin@Vtdks-MacBook-Pro.local>
2024-11-04 21:48:57 +0200

    add third lesson

---
+++ trecia-pamoka/script.js
@@ -0,0 +1,54 @@
+// 1. Uždavinys: Skaičių suma
+// Sukurk tris kintamuosius su skaičiaus tipo reikšmėmis. Apskaičiuok jų sumą ir priskirk ją naujam kintamajam. Atspausdink rezultatą.
+
+let a = 5;
+let b = 10;
+let c = 15;
+
+let suma = a + b + c;
+
+console.log(a + b + c);
+
+// 2. Uždavinys: Stačiakampio plotas
+// Sukurk du kintamuosius - ilgis ir plotis, priskirk jiems bet kokias reikšmes ir apskaičiuok stačiakampio plotą.
+
+let h = 10;
+let w = 7;
+
+console.log(h * w);
+
+// 3. Uždavinys: Kvadrato plotas
+// Sukurk kintamąjį ir priskirk jam bet kokį skaičių. Apskaičiuok kvadrato plotą ir jį atspausdink.
+
+let d = 4;
+let c = 4;
+
+console.log(d * c);
+
+// 4. Uždavinys: Vardo pasisveikinimas
+// Sukurk kintamąjį vardas ir priskirk jam kokį nors vardą. Programa turi pasisveikinti su tuo vardu. Pvz.: “Labas, Vardeni”.
+
+// 5. Uždavinys: Dvigubas skaičius
+// Sukurk kintamąjį skaicius, jam priskirk bet kokią skaitinę reikšmę. Programa turi atspausdinti dvigubą šio skaičiaus reikšmę.
+
+// 6. Uždavinys: Kūno masės indeksas
+// Sukurk kintamuosius ūgis ir svoris. Parašyk programą, kuri apskaičiuoja kūno masės indeksą.
+
+// 7. Uždavinys: Draugų skaičius
+// Sukurk kintamąjį drauguSkaicius, kuriame bus saugomas tavo draugų skaičius. Parašyk programą, kuri prideda 5 naujus draugus ir atspausdina bendrą draugų skaičių.
+
+// 8. Uždavinys: Kavos užsakymas
+// Sukurk kintamuosius kavosKiekis ir kavosKaina. Parašyk programą, kuri suskaičiuoja kavos užsakymo sumą ir ją atspausdina.
+
+// 9. Uždavinys: PVM skaičiavimas
+// Sukurk kintamuosius kainaBePVM ir pvm, kurie išsaugos prekės kainą ir PVM procentą (pvz., 21). Programa turi apskaičiuoti kainą su PVM.
+
+// 10. Uždavinys: Automobilio kuro sąnaudos
+// Sukurk kintamuosius nuvaziuotiKm, kuroSanaudos ir kuroKaina. Parašyk programą, kuri apskaičiuoja kiek kainuos nukeliauti nuvaziuotiKm su šiuo automobiliu.
+
+// 11. Uždavinys: Metų skaičius mėnesiais.
+// Sukurk kintamąjį metai, kuriame būtų saugomas skaičius, kiek metų. Programa turi apskaičiuoti, kiek tai sudaro mėnesių.
+
+// 12. Uždavinys: Temperatūros perskaičiavimas
+// Parašyk programą, kuri konvertuoja temperatūrą iš Celsijaus į Farenheito skalę. Sukurk kintamąjį temperaturaC ir priskirk jam bet kokią reikšmę,
+// o tada apskaičiuok temperatūrą Farenheito skalėje. Formulė: F = C * 9/5 + 32
